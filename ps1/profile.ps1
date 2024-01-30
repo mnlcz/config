051a62d102b0a24ba7f2ff9792d5119012955e9c
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-29 22:19:03 -0300

    Change posh theme

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh --init --shell pwsh --config C:\Users\manuc\Documents\PowerShell\Modules\oh-my-posh\themes\stelbent.minimal.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:\Users\manuc\Documents\PowerShell\Modules\oh-my-posh\themes\amro.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 Set-PSReadLineOption -PredictionViewStyle ListView
