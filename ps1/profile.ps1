df8cb109d5a619be9fb25d3fd4cd99b4bbac1fd0
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-31 19:45:37 -0300

    Added alias for Windows

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -1,4 +1,16 @@
-oh-my-posh --init --shell pwsh --config C:\Users\manuc\Documents\PowerShell\Modules\oh-my-posh\themes\star.omp.json | Invoke-Expression
-
-Import-Module -Name Terminal-Icons
+# Shell behaviour
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Appearance
+oh-my-posh --init --shell pwsh --config C:\Users\manuc\Documents\PowerShell\Modules\oh-my-posh\themes\star.omp.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+# Alias related
+Set-Alias -Name nvconf -Value NvimConfPath
+Set-Alias -Name psconf -Value PSUserPath
+Set-Alias -Name dotfiles -Value ConfigPath
+
+# Util funcs
+function NvimConfPath { Set-Location -Path ~/AppData/Local/nvim/lua/custom/ }
+function PSUserPath { Set-Location -Path ~/Documents/PowerShell/ }
+function ConfigPath { Set-Location -Path F:/Dev/Repos/Config/ }
