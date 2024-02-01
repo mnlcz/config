4f7493323d91ad8c60af822922e14b5d79c76110
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-01-31 22:19:31 -0300

    Now deletes everything and repopulates ./nvim/ directory

--- Sync.ps1
+++ Sync.ps1
@@ -2,4 +2,5 @@
 Copy-Item -Force -Path C:\Users\manuc\Documents\PowerShell\profile.ps1 -Destination ./ps1/
 
 # nvim
-Copy-Item -Path "C:\Users\manuc\AppData\Local\nvim\lua\custom\*" -Destination "./nvim" -Recurse -Force
+Remove-Item -Recurse -Force -Path ./nvim/*
+Copy-Item -Path "C:\Users\manuc\AppData\Local\nvim\lua\custom\*" -Destination "./nvim/" -Recurse -Force
