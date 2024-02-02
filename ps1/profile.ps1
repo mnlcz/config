4b568fb828dd54d978e93d92ff4cefc573ac89f9
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-02-02 09:30:22 -0300

    Added initial linux config

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -1,7 +1,8 @@
 # Shell behaviour
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-if ($IsWindows) {
+if ($IsWindows) 
+{
     # Appearance
     oh-my-posh --init --shell pwsh --config F:\Dev\Repos\Config\ps1\custom-themes\star.omp.json | Invoke-Expression
     Import-Module -Name Terminal-Icons
@@ -15,5 +16,10 @@ if ($IsWindows) {
     function NvimConfPath { Set-Location -Path ~/AppData/Local/nvim/lua/custom/ }
     function PSUserPath { Set-Location -Path ~/Documents/PowerShell/ }
     function ConfigPath { Set-Location -Path F:/Dev/Repos/Config/ }
+}
+elseif ($IsLinux)
+{
+    # Add composer global packages bin to PATH
+    $env:PATH += ":/home/mnlcz/.composer/vendor/bin"
 }
 
