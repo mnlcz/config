dc8f26e7710bc5a42630232196128b31d82593c0
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-03-09 21:14:11 -0300

    Remove format on save

--- nvim/configs/conform.lua
+++ nvim/configs/conform.lua
@@ -18,11 +18,11 @@ local options = {
     sh = { "shfmt" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 require("conform").setup(options)
