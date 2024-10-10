964eec2b6c49847cbf9f4f0a9387834686553271
MnLCz <manuel.lopez.cosmitz@gmail.com>
2024-10-10 06:21:08 -0300

    Add Neorg plugin with default configs

--- nvim/plugins/init.lua
+++ nvim/plugins/init.lua
@@ -1,115 +1,122 @@
 return {
-  ------------------ With opts in /configs ------------------
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	------------------ With opts in /configs ------------------
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  {
-    "NvChad/nvterm",
-    opts = require "configs.nvterm",
-  },
+	{
+		"NvChad/nvterm",
+		opts = require("configs.nvterm"),
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  ------------------ Inline configs ------------------
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = {
-  --     ensure_installed = {
-  --       "vim",
-  --       "lua",
-  --       "vimdoc",
-  --
-  --       -- web
-  --       "html",
-  --       "css",
-  --       "javascript",
-  --       "typescript",
-  --       "tsx",
-  --       "json",
-  --       "php",
-  --       "vue",
-  --       "http",
-  --
-  --       -- general langs
-  --       "ruby",
-  --       "python",
-  --       "c_sharp",
-  --       "c",
-  --       "rust",
-  --       "kotlin",
-  --
-  --       -- misc
-  --       "markdown",
-  --       "markdown_inline",
-  --       "latex",
-  --       "gitignore",
-  --       "yaml",
-  --       "xml",
-  --     },
-  --   },
-  -- },
+	{
+		"nvim-neorg/neorg",
+		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+		version = "*", -- Pin Neorg to the latest stable release
+		config = true, -- check https://dotfyle.com/plugins/nvim-neorg/neorg for possible configs
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      git = {
-        enable = true,
-      },
+	------------------ Inline configs ------------------
+	-- {
+	--   "nvim-treesitter/nvim-treesitter",
+	--   opts = {
+	--     ensure_installed = {
+	--       "vim",
+	--       "lua",
+	--       "vimdoc",
+	--
+	--       -- web
+	--       "html",
+	--       "css",
+	--       "javascript",
+	--       "typescript",
+	--       "tsx",
+	--       "json",
+	--       "php",
+	--       "vue",
+	--       "http",
+	--
+	--       -- general langs
+	--       "ruby",
+	--       "python",
+	--       "c_sharp",
+	--       "c",
+	--       "rust",
+	--       "kotlin",
+	--
+	--       -- misc
+	--       "markdown",
+	--       "markdown_inline",
+	--       "latex",
+	--       "gitignore",
+	--       "yaml",
+	--       "xml",
+	--     },
+	--   },
+	-- },
 
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-          },
-        },
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			git = {
+				enable = true,
+			},
 
-  ------------------ Added plugins ------------------
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+			renderer = {
+				highlight_git = true,
+				icons = {
+					show = {
+						git = true,
+					},
+				},
+			},
+		},
+	},
 
-  {
-    "lervag/vimtex",
-    lazy = false,
-    ft = "plaintex",
-    config = function()
-      -- Set the vimtex_viewer
-      vim.g.vimtex_view_method = "zathura"
-      -- vim.g.vimtex_view_general_viewer = "sioyek"
-      -- Set the vimtex_compiler_latexmk configuration
-      vim.g.vimtex_compiler_latexmk = {
-        out_dir = "build",
-      }
-    end,
-  },
+	------------------ Added plugins ------------------
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	{
+		"lervag/vimtex",
+		lazy = false,
+		ft = "plaintex",
+		config = function()
+			-- Set the vimtex_viewer
+			vim.g.vimtex_view_method = "zathura"
+			-- vim.g.vimtex_view_general_viewer = "sioyek"
+			-- Set the vimtex_compiler_latexmk configuration
+			vim.g.vimtex_compiler_latexmk = {
+				out_dir = "build",
+			}
+		end,
+	},
 
-  {
-    "ellisonleao/glow.nvim",
-    config = true,
-    cmd = "Glow",
-  },
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
+
+	{
+		"ellisonleao/glow.nvim",
+		config = true,
+		cmd = "Glow",
+	},
 }
