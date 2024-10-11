835ee6c2eea30cb2f1372e99ca826be5b7140a5a
MnLCz <manuel.lopez.cosmitz@gmail.com>
2024-10-11 01:01:37 -0300

    Apply Lazy best practices

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,23 +1,22 @@
-local options = {
-    load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manage Neorg workspaces
-            config = {
-                workspaces = {
-                    notes = "~/repos/Neorg/",
-                },
-                default_workspace = "notes",
-            },
-        },
+local load = {
+  ["core.defaults"] = {},  -- Loads default behaviour
+  ["core.concealer"] = {}, -- Adds pretty icons to your documents
+  ["core.dirman"] = {      -- Manage Neorg workspaces
+    config = {
+      workspaces = {
+        notes = "~/repos/Neorg/",
+      },
+      default_workspace = "notes",
     },
+  },
 }
 
 return {
-    "nvim-neorg/neorg",
-    lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = function()
-        require("neorg").setup(options)
-    end,
+  "nvim-neorg/neorg",
+  lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  version = "*", -- Pin Neorg to the latest stable release
+  ft = "norg",
+  opts = {
+    load = load,
+  }
 }
