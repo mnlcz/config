430457194a1b1830ddb44433f1bdfe7097fbbf64
Manuel Lopez Cosmitz <manuel.lopez.cosmitz@gmail.com>
2024-10-20 18:41:55 -0300

    Add hyprland config lsp

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -5,7 +5,7 @@ local servers = {
   "jdtls", "html", "cssls", "perlnavigator", "solargraph", "pylsp", "ts_ls",
 }
 
--- Overrides
+----------------------------- Overrides -----------------------------
 local lua_ls = function()
   require 'lspconfig'.lua_ls.setup {
     on_init = function(client)
@@ -66,6 +66,21 @@ local omnisharp = function()
   }
 end
 
+----------------------------- Manual -----------------------------
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+		pattern = {"*.hl", "hypr*.conf"},
+		callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						cmd = {"hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+		end
+})
+
+----------------------------- Setup -----------------------------
 return {
   "williamboman/mason-lspconfig.nvim",
   dependencies = { "mason.nvim" },
