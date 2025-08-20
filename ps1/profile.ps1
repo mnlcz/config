0e1a72b3943196e95ba548c1e2de7014b3b87915
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-08-20 00:21:56 -0300

    Add new alias

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -12,9 +12,13 @@ if ($IsWindows)
     # Alias related
     Set-Alias -Name config -Value ConfigPath
     Set-Alias -Name v -Value nvim
+    Set-Alias -Name vps1 -Value PS1Config
+    Set-Alias -Name vnv -Value NvimConfig
 
     # Util funcs
     function ConfigPath { Set-Location -Path F:/Dev/Repos/Config/ }
+    function PS1Config { nvim ~/Documents/PowerShell/profile.ps1 }
+    function NvimConfig { nvim ~/AppData/Local/nvim/ }
 }
 elseif ($IsLinux)
 {
