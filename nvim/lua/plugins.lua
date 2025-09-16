e874ecfc5f0a1559be39f695deb88d53880da3ed
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-09-16 03:01:12 +0000

    Cleanup and treesitter setup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,8 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter",            version = "master" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/bettervim/yugen.nvim" },
 })
 
@@ -32,3 +34,49 @@ require("conform").setup({
 })
 
 require("gitsigns").setup({ signcolumn = false })
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "html", "css", "javascript",
+    "json", "php", "http", "python", "c_sharp", "rust", "gitignore", "yaml", "xml", "java" },
+  auto_install = true,
+  sync_install = false,
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<Leader>ss",    -- Start selection
+      node_incremental = "<Leader>si",  -- Selection increment
+      scope_incremental = "<Leader>sc", -- Scope increment
+      node_decremental = "<Leader>sd",  -- Selection decrement
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+        ["ii"] = "@conditional.inner",
+        ["ai"] = "@conditional.outer",
+        ["il"] = "@loop.inner",
+        ["al"] = "@loop.outer",
+      },
+
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V',  -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+
+      include_surrounding_whitespace = true,
+    },
+  },
+})
