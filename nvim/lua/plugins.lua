db274ec9aef7a17096e31c9842961d3cd57884fd
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-09-17 10:33:13 +0000

    Add markdown formatter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -126,10 +126,11 @@ require("nvim-autopairs").setup({
 
 require("conform").setup({
   formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettier" },
     css = { "prettier" },
     html = { "prettier" },
+    javascript = { "prettier" },
+    markdown = { "prettier" },
+    lua = { "stylua" },
     tex = { "latexindent" },
   },
 })
