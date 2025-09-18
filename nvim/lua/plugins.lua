fa6edc04b58995b7c845c3fa708fb5a122fc7908
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-09-18 17:30:43 +0000

    Change theme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -97,7 +97,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/lervag/vimtex" },
   -- { src = "https://github.com/folke/which-key.nvim" }, -- Not working for me on nvim unstable
-  { src = "https://github.com/bettervim/yugen.nvim" },
+  { src = "https://github.com/metalelf0/black-metal-theme-neovim" },
 })
 
 -- No config
