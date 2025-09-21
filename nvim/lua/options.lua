e99e17cacdc0919c63ecdd76367be6f3fef50068
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-09-21 10:20:39 +0000

    Add extra lang for spell checking
    Theo automatically download new spell files nvim has to be capable of editing its 'runtimepath' dir (tldr: for me, sudo was required)

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -53,9 +53,10 @@ vim.o.virtualedit = "block" -- Makes empty character cells available in block mo
 -------------------------- Edition ----------------------------
 vim.o.clipboard = "unnamedplus" -- System clipboard
 vim.o.inccommand = "split"      -- Show command output preview in different buffer
+
 -- Spell check
-vim.o.spelllang = 'en_us'
 vim.o.spell = true
+vim.o.spelllang = "en_us,es,ru"
 
 ---------------------------- QOL ------------------------------
 vim.o.ignorecase = true -- Ignore case for command autocomplete ease of use
