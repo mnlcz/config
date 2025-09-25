e86a2180c8127600b96f5ddfb361ef1a5853e8e9
mnlcz <manuel.lopez.cosmitz@gmail.com>
2025-09-25 17:42:59 +0000

    Change map to avoid overwriting <CR> functionality

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,6 +1,6 @@
 local map = vim.keymap.set
 
-map("i", "<C-m>", function()
+map("i", "<A-m>", function()
   vim.api.nvim_put({"\\(\\)"}, "c", true, true)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Left><Left>", true, false, true), "n", false)
 end, { desc = "Insert inline math section", buffer = true })
