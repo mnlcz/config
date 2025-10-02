733f2232ac4207c1f31d0cbf7ab8493e4c030a5b
MnLCz <manuel.lopez.cosmitz@gmail.com>
2025-10-02 13:35:12 -0300

    Change sessions order

--- scripts/init-tmux.sh
+++ scripts/init-tmux.sh
@@ -4,6 +4,14 @@ if [ -n "$TMUX" ]; then
     exit 0
 fi
 
+# main session
+if tmux has-session -t 'main' 2>/dev/null; then
+    tmux attach-session -t main
+else
+    tmux new-session -d -s 'main' -n 'bash'
+    tmux send-keys -t main:bash 'cd ~' Enter
+fi
+
 # config session
 if ! tmux has-session -t 'config' 2>/dev/null; then
     tmux new-session -d -s 'config' -n 'nvim'
@@ -13,11 +21,4 @@ if ! tmux has-session -t 'config' 2>/dev/null; then
     tmux send-keys -t config:bash 'cd ~/repos/config && git status' Enter
 fi
 
-# main session
-if tmux has-session -t 'main' 2>/dev/null; then
-    tmux attach-session -t main
-else
-    tmux new-session -d -s 'main' -n 'bash'
-    tmux send-keys -t main:bash 'cd ~' Enter
-    tmux attach-session -t main
-fi
+tmux attach-session -t main
