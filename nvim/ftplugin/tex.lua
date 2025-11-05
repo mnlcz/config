db1f42975939e1932f7f06ec36fb73cd012e83d4
MnLCz <mnlcz@tuta.io>
2025-11-05 07:16:33 -0300

    Tex mappings

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,6 +1,13 @@
 local map = vim.keymap.set
 
 map("i", "<A-m>", function()
-  vim.api.nvim_put({"\\(\\)"}, "c", true, true)
+  vim.api.nvim_put({ "\\(\\)" }, "c", true, true)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Left><Left>", true, false, true), "n", false)
 end, { desc = "Insert inline math section", buffer = true })
+
+map("i", "<A-e>", function()
+  vim.api.nvim_put({ "\\begin{equation}" }, "l", true, true)
+  vim.api.nvim_put({ "    " }, "l", true, true)
+  vim.api.nvim_put({ "\\end{equation}" }, "l", true, true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Up><Right><Right><Right><Right>", true, false, true), "n", false)
+end, { desc = "Insert equation block", buffer = true })
