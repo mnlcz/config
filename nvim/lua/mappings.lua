64bcdf8882aaf8e5e152a1242e02132d7d586da9
mnlcz <mnlcz@tuta.io>
2026-03-11 18:33:03 -0300

    Cleanup

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -39,7 +39,7 @@ map("n", "[t", "<cmd>tabprevious<CR>", { desc = "Jumps to previous tab" })
 map("n", "<leader>sc", function() vim.o.spell = not vim.o.spell end, { desc = "Toggle spellcheck" })
 
 -- Dev
-map("n", "<leader>r", "<Cmd>:.lua<CR>", { desc="Run Lua code inline" })
+map("n", "<leader>r", "<Cmd>:.lua<CR>", { desc = "Run Lua code inline" })
 
 ---------------------------- LSP ------------------------------
 map("n", "<leader>lf", lsp.format, { desc = "[LSP] Format code" })
@@ -71,24 +71,3 @@ map("n", "<leader>fc", telescope.spell_suggest, { desc = "Telescope spell sugges
 -- Markdown
 -- map("n", "<leader>mp", "<cmd>Glow<CR>", { desc = "Markdown file preview with Glow" })
 map("n", "<leader>mr", "<cmd>:RenderMarkdown toggle<CR>", { desc = "Toggle markdown rendering" })
-
----------------------------- INACTIVE ------------------------------
--- Neorg
--- map("n", "<leader>njt", "<cmd>Neorg journal today<CR>", { desc = "[neorg] Open today's note" })
--- map("n", "<leader>njy", "<cmd>Neorg journal yesterday<CR>", { desc = "[neorg] Open yesterday's note" })
--- map("n", "<leader>nw", "<cmd>Neorg workspace notes<CR>", { desc = "[neorg] Open main workspace index" })
--- map("n", "<localleader>is", "<cmd>Neorg generate-workspace-summary<CR>", { desc = "[neorg] Generate workspace summary" })
--- map("n", "<localleader>im", "<cmd>Neorg inject-metadata<CR>", { desc = "[neorg] Inject metadata" })
--- map("n", "<localleader>c", "<cmd>Neorg toc<CR>", { desc = "[neorg] Open table of contents" })
--- map("n", "<leader>fnh", "<Plug>(neorg.telescope.search_headings)", { desc = "[neorg] Telescope find neorg headings" })
--- map("n", "<leader>fna", "<Plug>(neorg.telescope.find_linkable)", { desc = "[neorg] Telescope find any linkable" })
--- map("n", "<leader>fnf", "<Plug>(neorg.telescope.find_norg_files)", { desc = "[neorg] Telescope find files" })
-
--- nvim-cmp (configured in ./plugins/nvim-cmp.lua)
--- mapping = cmp.mapping.preset.insert({
---   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
---   ['<C-f>'] = cmp.mapping.scroll_docs(4),
---   ['<C-Space>'] = cmp.mapping.complete(),
---   ['<C-e>'] = cmp.mapping.abort(),
---   ['<CR>'] = cmp.mapping.confirm({ select = true }),
--- }),
