2a51011c236ba173ca6872800df8ea32edbcaa43
mnlcz <mnlcz@tuta.io>
2026-03-11 23:47:35 -0300

    Add media keybindings

--- scripts/gnome-restore-keybindings.sh
+++ scripts/gnome-restore-keybindings.sh
@@ -4,5 +4,6 @@ DIR="$CONFIG_DIR/wm/mutter"
 
 dconf load /org/gnome/desktop/wm/keybindings/ < "$DIR/wm-keybindings.conf"
 dconf load /org/gnome/shell/keybindings/ < "$DIR/shell-keybindings.conf"
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < "$DIR/media-keys.conf"
 
 echo "Keybindings restored."
--- scripts/gnome-save-keybindings.sh
+++ scripts/gnome-save-keybindings.sh
@@ -4,5 +4,6 @@ DIR="$CONFIG_DIR/wm/mutter"
 
 dconf dump /org/gnome/desktop/wm/keybindings/ > "$DIR/wm-keybindings.conf"
 dconf dump /org/gnome/shell/keybindings/ > "$DIR/shell-keybindings.conf"
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > "$DIR/media-keys.conf"
 
 echo "Keybindings saved."
