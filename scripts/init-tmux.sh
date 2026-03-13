6cc840ee32053979f245c3e8f65643e422a04687
mnlcz <mnlcz@tuta.io>
2026-03-13 09:19:55 -0300

    Add window to 'code' session

--- scripts/init-tmux.sh
+++ scripts/init-tmux.sh
@@ -12,13 +12,16 @@ else
     tmux send-keys -t main:bash 'cd ~' Enter
 fi
 
-# config session
+# dev session
 if ! tmux has-session -t 'code' 2>/dev/null; then
     tmux new-session -d -s 'code' -n "shell"
     tmux send-keys -t code:shell 'cd $SOURCE_DIR ; x ls' Enter
     # 2nd window
     tmux new-window -t code -n 'runner'
     tmux send-keys -t code:runner 'cd $SOURCE_DIR ; x ls' Enter
+    # 3rd window
+    tmux new-window -t code -n 'podman'
+    tmux send-keys -t code:podman 'cd $SOURCE_DIR ; podman ps -a' Enter
 fi
 
 tmux attach-session -t main
