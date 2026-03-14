cfaa08153b85152e62d5ea35202151d279368b0d
mnlcz <mnlcz@tuta.io>
2026-03-14 16:41:51 +0000

    Add distro check

--- scripts/init-tmux.sh
+++ scripts/init-tmux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OS=$(cat /etc/os-release | grep "^NAME=")
+
 if [ -n "$TMUX" ]; then
     exit 0
 fi
@@ -12,16 +14,26 @@ else
     tmux send-keys -t main:bash 'cd ~' Enter
 fi
 
-# dev session
-if ! tmux has-session -t 'code' 2>/dev/null; then
-    tmux new-session -d -s 'code' -n "shell"
-    tmux send-keys -t code:shell 'cd $SOURCE_DIR ; x ls' Enter
-    # 2nd window
-    tmux new-window -t code -n 'runner'
-    tmux send-keys -t code:runner 'cd $SOURCE_DIR ; x ls' Enter
-    # 3rd window
-    tmux new-window -t code -n 'podman'
-    tmux send-keys -t code:podman 'cd $SOURCE_DIR ; podman ps -a' Enter
+if [[ "$OS" == *"Vanilla OS"* ]]; then
+	if ! tmux has-session -t 'code' 2>/dev/null; then
+		tmux new-session -d -s 'code' -n "shell"
+		tmux send-keys -t code:shell 'cd $SOURCE_DIR ; ls -lah' Enter
+		# 2nd window
+		tmux new-window -t code -n 'runner'
+		tmux send-keys -t code:runner 'cd $SOURCE_DIR ; ls -lah' Enter
+	fi
+else
+	# dev session
+	if ! tmux has-session -t 'code' 2>/dev/null; then
+		tmux new-session -d -s 'code' -n "shell"
+		tmux send-keys -t code:shell 'cd $SOURCE_DIR ; x ls' Enter
+		# 2nd window
+		tmux new-window -t code -n 'runner'
+		tmux send-keys -t code:runner 'cd $SOURCE_DIR ; x ls' Enter
+		# 3rd window
+		tmux new-window -t code -n 'podman'
+		tmux send-keys -t code:podman 'cd $SOURCE_DIR ; podman ps -a' Enter
+	fi
 fi
 
 tmux attach-session -t main
