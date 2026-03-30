cf8324ab32a8ca96591afc91105041fd41dd1c1b
mnlcz <mnlcz@tuta.io>
2026-03-29 23:08:20 -0300

    Fix auto attachment

--- scripts/init-tmux.sh
+++ scripts/init-tmux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 OS=$(cat /etc/os-release | grep "^NAME=")
 
@@ -6,32 +7,29 @@ if [ -n "$TMUX" ]; then
     exit 0
 fi
 
-# main session
-if tmux has-session -t 'main' 2>/dev/null; then
-    tmux attach-session -t main
-else
-    tmux new-session -d -s 'main' #-n 'bash'
-    tmux send-keys -t main:bash 'cd ~ ; clear ; fastfetch' Enter
+# Create main session if it doesn't exist
+if ! tmux has-session -t 'main' 2>/dev/null; then
+    tmux new-session -d -s 'main'
+    tmux send-keys -t main 'cd ~ ; clear ; fastfetch' Enter
 fi
 
-if [[ "$OS" == *"Vanilla OS"* ]]; then
-	if ! tmux has-session -t 'code' 2>/dev/null; then
-		tmux new-session -d -s 'code' -n "shell"
-		tmux send-keys -t code:shell 'cd $SOURCE_DIR ; ls -lah' Enter
-		# 2nd window
-		tmux new-window -t code -n 'runner'
-		tmux send-keys -t code:runner 'cd $SOURCE_DIR ; ls -lah' Enter
-	fi
-else
-	# dev session
-	if ! tmux has-session -t 'code' 2>/dev/null; then
-		tmux new-session -d -s 'code' -n "shell"
-		tmux send-keys -t code:shell 'cd $SOURCE_DIR ; x ls' Enter
-		# 2nd window
-		tmux new-window -t code -n 'runner'
-		tmux send-keys -t code:runner 'cd $SOURCE_DIR ; x ls' Enter
-		# 3rd window
-		tmux new-window -t code -n 'podman'
-		tmux send-keys -t code:podman 'cd $SOURCE_DIR ; podman ps -a' Enter
-	fi
+# Create code session based on OS
+if ! tmux has-session -t 'code' 2>/dev/null; then
+    if [[ "$OS" == *"Vanilla OS"* ]]; then
+        tmux new-session -d -s 'code' -n "shell"
+        tmux send-keys -t code:shell 'cd $SOURCE_DIR ; clear ; ls -lah' Enter
+        tmux new-window -t code -n 'runner'
+        tmux send-keys -t code:runner 'cd $SOURCE_DIR ; clear ; ls -lah' Enter
+    else
+        tmux new-session -d -s 'code' -n "shell"
+        tmux send-keys -t code:shell 'cd $SOURCE_DIR ; clear ; x ls' Enter
+        tmux new-window -t code -n 'editor'
+        tmux send-keys -t code:editor 'cd $SOURCE_DIR ; clear ; nvim -V1 -v' Enter
+        tmux new-window -t code -n 'runner'
+        tmux send-keys -t code:runner 'cd $SOURCE_DIR ; clear ; x ls' Enter
+        tmux new-window -t code -n 'podman'
+        tmux send-keys -t code:podman 'cd $SOURCE_DIR ; clear ; podman ps -a' Enter
+    fi
 fi
+
+tmux attach-session -t main
