2730c72a1d04af56c8d5b7b17b2bf08d9c06b40e
mnlcz <mnlcz@tuta.io>
2026-03-29 23:15:04 -0300

    Fix visual mode highlight color

--- editor/nvim/lua/autocmds.lua
+++ editor/nvim/lua/autocmds.lua
@@ -1,18 +1,23 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
 })
 vim.cmd("set completeopt+=noselect")
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    vim.api.nvim_set_hl(0, 'Visual', {
-      bg = '#bcbcbc', -- Light blue/purple
-      fg = '#000000'  -- Black text
-    })
-  end,
+	callback = function()
+		vim.api.nvim_set_hl(0, "Visual", {
+			bg = "#bcbcbc", -- Light blue/purple
+			fg = "#000000", -- Black text
+		})
+	end,
+})
+
+vim.api.nvim_set_hl(0, "Visual", {
+	bg = "#bcbcbc",
+	fg = "#000000",
 })
