5eb98048af36be99aa3d4f44592e8307247b602d
mnlcz <mnlcz@tuta.io>
2026-04-24 07:33:50 -0300

    Use system font by configuring fontsrv

--- shells/.bashrc.d/plan9.sh
+++ shells/.bashrc.d/plan9.sh
@@ -1,5 +1,8 @@
 # Requires fusermount (or fusermount3 aliased) with user_allow_other uncommented in /etc/fuse.conf
 acme() {
-    # mkdir -p /mnt/acme
-    $PLAN9/bin/acme -m /mnt/acme "$@" &
+    # mkdir -p /mnt/acme ; sudo chown $(whoami): /mnt/acme
+    # mkdir -p /mnt/font ; sudo chown $(whoami): /mnt/font
+    fontsrv -m /mnt/font &
+    sleep 1 # give fontsrv a moment to start
+    $PLAN9/bin/acme -m /mnt/acme -f /mnt/font/RedHatMono-Regular/13a/font "$@" &
 }
