1cb6c8ecc3c4a77c1dd1abc0cf6034df5642a0c2
mnlcz <mnlcz@tuta.io>
2026-04-27 17:41:25 -0300

    Initial dwlb customization

---
+++ wm/dwl/dwlb/status.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+get_network() {
+    if ip route get 1.1.1.1 &>/dev/null 2>&1; then
+        echo "󰈀"
+    else
+        echo "󰈁"
+    fi
+}
+
+get_memory() {
+    free -h | awk '/Mem:/ {print $3"/"$2}'
+}
+
+get_clock() {
+    date '+%a %d %b %H:%M'
+}
+
+while true; do
+    echo "$(get_network)  $(get_memory)  $(get_clock)"
+    sleep 5
+done
