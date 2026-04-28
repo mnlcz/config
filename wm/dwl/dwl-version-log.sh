ac742118b73dbb3eab4293a672a93ddd53a34879
mnlcz <mnlcz@tuta.io>
2026-04-28 06:53:36 -0300

    Add clipboard packages to version log

--- wm/dwl/dwl-version-log.sh
+++ wm/dwl/dwl-version-log.sh
@@ -6,15 +6,17 @@ meson_version() {
 }
 
 {
+	echo "bemenu: $(bemenu --version)"
+	echo "cliphist: $(cliphist version 2>&1 | head -n 1)"
     echo "dwl: $(dwl -v 2>&1)"
     echo "dwlb: $(dwlb -v 2>&1)"
-    echo "bemenu: $(bemenu --version)"
+	echo "fcft: $(pkg-config --modversion fcft)"
     echo "foot: $(foot -v)"
+	echo "grim: $(meson_version /usr/local/src/grim)"
     echo "j4-dmenu-desktop: $(j4-dmenu-desktop --version)"
     echo "mako: $(meson_version /usr/local/src/mako)"
-    echo "grim: $(meson_version /usr/local/src/grim)"
-    echo "slurp: $(meson_version /usr/local/src/slurp)"
-    echo "fcft: $(pkg-config --modversion fcft)"
+    echo "slurp: $(meson_version /usr/local/src/slurp)"   
     echo "wlroots: $(pkg-config --modversion wlroots-0.18)"
+	echo "xsel: $(xsel --version)"
     echo "Generated: $(date)"
 } > "$VERSIONS_FILE"
