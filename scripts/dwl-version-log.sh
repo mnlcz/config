6e775ef5c76014e4983550a94d35181a0a0d24e4
mnlcz <mnlcz@tuta.io>
2026-04-27 23:51:09 -0300

    New snapshot

--- scripts/dwl-version-log.sh
+++ scripts/dwl-version-log.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 VERSIONS_FILE="$CONFIG_DIR/wm/dwl/VERSIONS"
 
+meson_version() {
+    grep "version:" "$1/meson.build" | head -n1 | grep -o "'[^']*'" | tr -d "'"
+}
+
 {
     echo "dwl: $(dwl -v 2>&1)"
     echo "dwlb: $(dwlb -v 2>&1)"
     echo "bemenu: $(bemenu --version)"
+    echo "j4-dmenu-desktop: $(j4-dmenu-desktop --version)"
+    echo "mako: $(meson_version /usr/local/src/mako)"
+    echo "grim: $(meson_version /usr/local/src/grim)"
+    echo "slurp: $(meson_version /usr/local/src/slurp)"
     echo "fcft: $(pkg-config --modversion fcft)"
     echo "wlroots: $(pkg-config --modversion wlroots-0.18)"
     echo "Generated: $(date)"
