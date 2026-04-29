489dcb578e2182baca724ab75bb469879595a712
mnlcz <mnlcz@tuta.io>
2026-04-29 08:37:37 -0300

    Change patch path

--- wm/dwl/dwl-patches-log.sh
+++ wm/dwl/dwl-patches-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PATCHES_FILE="$CONFIG_DIR/wm/dwl/PATCHES"
 
-ls -1 /usr/local/src/dwl | grep '\.patch' > "$PATCHES_FILE"
+ls -1 /usr/local/src/dwl/patches | grep '\.patch' > "$PATCHES_FILE"
