f4a1cb3bcfe7374f042084df2da3421c26183f57
mnlcz <mnlcz@tuta.io>
2026-05-06 16:36:39 -0300

    Use home dump
    The repo's acme.dump is used as a base (copy to ~ when needed)

--- scripts/launch-acme.sh
+++ scripts/launch-acme.sh
@@ -14,4 +14,4 @@ fi
 cat $PLAN9/plumb/basic | $PLAN9/bin/9p write plumb/rules
 # Note: changing font requires ignoring previous dump
 # SHELL=$PLAN9/bin/rc $PLAN9/bin/acme -m /mnt/acme -f /mnt/font/TempleOSRegular/10a/font "$@" &
-SHELL=$PLAN9/bin/rc $PLAN9/bin/acme -l $CONFIG_DIR/editor/acme.dump -m /mnt/acme -f /mnt/font/TempleOSRegular/10a/font "$@" &
+SHELL=$PLAN9/bin/rc $PLAN9/bin/acme -l $HOME/acme.dump -m /mnt/acme -f /mnt/font/TempleOSRegular/10a/font "$@" &
