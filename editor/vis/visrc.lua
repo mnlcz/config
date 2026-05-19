757b4b9e4efd6a708b2b1fbf390f8d4d671e49d4
mnlcz <mnlcz@tuta.io>
2026-05-19 08:55:45 -0300

    Fix

--- editor/vis/visrc.lua
+++ editor/vis/visrc.lua
@@ -3,22 +3,19 @@ require("vis")
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme acme")
 
-	-- Indenting (global defaults)
-	vis:command("set expandtab")
-	vis:command("set tabwidth 4")
-
 	-- Search
 	vis:command("set ignorecase")
 
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Indenting (global defaults)
+	vis:command("set expandtab")
+	vis:command("set tabwidth 4")
+
 	-- Line numbers
 	vis:command("set number")
 	vis:command("set relativenumbers")
-
-	-- Wrap
-	vis:command("set nowrap")
 
 	-- Per-filetype indentation (2 spaces for web/config files)
 	local ft = win.syntax
