2e857e27f13c24d5955ddbdf8d8f46d6b420d72f
MnLCz <mnlcz@tuta.io>
2026-05-27 22:09:44 -0300

    Add dir vars

--- shells/.shrc.d/0dirs.sh
+++ shells/.shrc.d/0dirs.sh
@@ -1,6 +1,8 @@
 export CONF="$HOME/Projects/config"
 export PROJ="$HOME/Projects"
-export SCRIPTS="$HOME/Projects/scripts" # local system scripts
-export CSCRIPTS="$CONF/scripts" # config scripts
+export SCRIPTS="$HOME/Projects/scripts"                        # local system scripts
+export CSCRIPTS="$CONF/scripts"                                # config scripts
+export BSDSCRIPTS="$HOME/Projects/config/freebsd/bin"          # FreeBSD scripts
 export TEMPLATES="$CONF/templates"
 export TODO="$HOME/Downloads/todo.txt"
+export DWLCONF="$CONF/wm/dwl"
